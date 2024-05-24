6ee7cd497c20e2af16c9d42f038155b8bacf6c15
Z24B <zivha.beharie@gmail.com>
2024-05-24 05:22:00 -0700

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
 from flask import Flask
+from os import getenv
 from models import storage
-from api.v1.views import app_views
-import os
+from api.v1 import app_views
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close storage session"""
+def teardown_appcontext(exception):
+    """Close the storage session"""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
