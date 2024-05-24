f71e6182d53d1b95e52912506ddc7de0aa4ab71c
Z24B <zivha.beharie@gmail.com>
2024-05-24 04:40:08 -0700

    Task 3

--- reset_database.py
+++
@@ -1,9 +0,0 @@
-# reset_database.py
-from models.base_model import Base
-from db_config import engine  # Import the engine variable from db_config.py
-
-# Drop existing tables
-Base.metadata.drop_all(engine)
-
-# Recreate tables with updated schema
-Base.metadata.create_all(engine)
